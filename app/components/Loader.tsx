cf3567b5dddadde9f47bf11d2bdd4d18bf703288
SBMcodes <shreyankumar212@gmail.com>
2024-10-03 20:48:10 +0530

    Loader component

---
+++ app/components/Loader.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+import { PuffLoader } from "react-spinners";
+
+const Loader = () => {
+  return <PuffLoader size={60} color="red" />;
+};
+
+export default Loader;
--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Loader from "./components/Loader";
 
 const Loading = () => {
   return (
     <div className="absolute top-0 left-0 w-[100vw] h-[100vh] flex justify-center items-center">
-      <div className="text-[38px] text-pink-600">Loading...</div>
+      <Loader />
     </div>
   );
 };
